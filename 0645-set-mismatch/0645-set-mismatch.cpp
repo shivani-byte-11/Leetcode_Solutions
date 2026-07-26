36306e07a77ee24a477760d4c8dd468ce2277f77
Shivani <shivanimandal908@gmail.com>
2026-07-26 21:35:09 +0530

    Time: 0 ms (100%), Space: 25.1 MB (88.85%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        long long n= nums.size();
+    // S - Sn = x-y
+    // S2 - S2n
+        long long Sn = (n*(n+1))/2;
+        long long S2n = (n*(n+1)*(2*n+1))/6;
+        long long S=0, S2=0;
+        for(int i=0; i<n; i++){
+            S += nums[i];
+            S2 += (long long)nums[i] * (long long)nums[i];
+        }
+        long long val1 = S - Sn;   // x-y
+        long long val2 = S2 - S2n;
+        val2 = val2/val1;      // x+y
+        long long x = (val1+val2)/2;
+        long long y = x-val1;
+        return {(int)x, (int)y};
+    }
+    
+};
